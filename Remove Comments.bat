aa98875dcabc4e62a11bca4cc9835ffe9c4fd5d7
robinwage <34686041+robinwage@users.noreply.github.com>
2024-11-09 14:41:15 +0100

    asd

--- Remove Comments.bat
+++ Remove Comments.bat
@@ -1,17 +1,17 @@
 @echo off
-:: This batch file removes all comments from all .html and .css files in the current directory (where the .bat file is located).
+:: This batch file removes all comments from .html and .css files in the current directory.
 
 :: Get the directory of the batch file
 set "DIR=%~dp0"
 
 :: Loop through all .html files in the current directory and remove comments
 for %%f in (%DIR%*.html) do (
-    powershell -Command "(Get-Content '%%f') -replace '<!--.*?-->', '' | Set-Content '%%f'"
+    powershell -Command "(Get-Content '%%f' -Raw) -replace '<!--.*?-->', '' | Set-Content '%%f'"
 )
 
 :: Loop through all .css files in the current directory and remove comments
 for %%f in (%DIR%*.css) do (
-    powershell -Command "(Get-Content '%%f') -replace '/\*.*?\*/', '' | Set-Content '%%f'"
+    powershell -Command "(Get-Content '%%f' -Raw) -replace '/\*.*?\*/', '' | Set-Content '%%f'"
 )
 
 echo All comments have been removed from HTML and CSS files in the current directory.
